ceee447ab7ddcc91000d81b337706dfe85c04dcb
Aakarsh Panwar <panwaraakarsh34@gmail.com>
2026-03-19 22:45:06 +0530

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,7 @@
--- Initialization script for Ansh's Postgres DB
--- This script automatically runs on the first DB startup
-
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     full_name VARCHAR(150) NOT NULL
 );
 
 -- Insert a default user to ensure there's dummy data
-INSERT INTO users (full_name) VALUES ('Ansh Negi Initial User');
+INSERT INTO users (full_name) VALUES ('Aakarsh Panwar Initial User');
